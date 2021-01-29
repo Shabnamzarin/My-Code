c40547ece7f251468f804bf867e70ab96f537ad6
Shabnam <Shabnam@192.168.0.23>
2021-01-28 22:26:46 -0500

    First Commit

---
+++ firstCode/src/firstCode/P0.java
@@ -0,0 +1,11 @@
+package firstCode;
+
+public class P0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+         System.out.println("Hello World");
+	}//method
+
+}//class
---
+++ firstCode/src/module-info.java
@@ -0,0 +1,2 @@
+module firstCode {
+}
